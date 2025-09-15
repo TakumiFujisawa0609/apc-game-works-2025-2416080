d7d7545c7e046d2bb3c836bf52ac15795da61313
kinoko0914 <ryusi.yamaguchi0914@gmail.com>
2025-09-16 03:01:23 +0900

    もろもろ追加

---
+++ クロノエクスプローラー/src/Application.cpp
@@ -0,0 +1,162 @@
+#include <DxLib.h>
+//#include <EffekseerForDXLib.h>
+//
+//#include "Utility/FrameRate.h"
+//
+#include "Manager/ResourceManager.h"
+#include "Manager/InputManager.h"
+#include "Manager/SceneManager.h"
+#include "Manager/SoundManager.h"
+
+#include "Application.h"
+
+Application* Application::instance_ = nullptr;
+
+const std::string Application::PATH_IMAGE = "Data/Image/";
+const std::string Application::PATH_MODEL = "Data/Model/";
+const std::string Application::PATH_EFFECT = "Data/Effect/";
+const std::string Application::PATH_SOUND = "Data/Sound/";
+
+void Application::CreateInstance(void)
+{
+	if (instance_ == nullptr)
+	{
+		instance_ = new Application();
+	}
+	instance_->Init();
+}
+
+Application& Application::GetInstance(void)
+{
+	return *instance_;
+}
+
+void Application::Init(void)
+{
+
+	// アプリケーションの初期設定
+	SetWindowText("春コン2025チームC");
+
+	// ウィンドウサイズ
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 32);
+	ChangeWindowMode(true);
+
+	// DxLibの初期化
+	SetUseDirect3DVersion(DX_DIRECT3D_11);
+	isInitFail_ = false;
+	if (DxLib_Init() == -1)
+	{
+		isInitFail_ = true;
+		return;
+	}
+
+	SetWaitVSyncFlag(0); // 垂直同期無効化
+	//frameRate_ = new FrameRate; // フレームレート
+
+	//Effekseerの初期化を行う
+	/*InitEffekseer();*/
+
+	// キー制御初期化
+	SetUseDirectInputFlag(true);
+	InputManager::CreateInstance();
+
+	//// リソース管理初期化
+	ResourceManager::CreateInstance();
+	////音の初期化
+	SoundManager::CreateInstance();
+	// シーン管理初期化
+	SceneManager::CreateInstance();
+
+}
+
+void Application::Run(void)
+{
+	//
+	auto& inputManager = InputManager::GetInstance();
+	auto& sceneManager = SceneManager::GetInstance();
+	//
+		// ゲームループ
+	while (ProcessMessage() == 0 && isGameEnd_ == false)
+	{
+		//		frameRate_->Update(); // フレームレート更新
+		//
+		//		if (frameRate_->GetLimitFrameRate() == true)
+		//		{
+		//			frameRate_->SetFrameRate();
+#ifdef _DEBUG
+			// デバッグ時のみ、Escapeキー入力時にゲーム終了処理
+		SetGameEnd((CheckHitKey(KEY_INPUT_ESCAPE) == 1) ? true : false);
+#endif
+		inputManager.Update();
+		sceneManager.Update();
+
+		sceneManager.Draw();
+
+		/*frameRate_->Draw();*/
+		ScreenFlip();
+	}
+	//
+	//	}
+
+}
+
+void Application::Destroy(void)
+{
+	// フレームレート制御 解放処理
+	delete frameRate_;
+
+
+	InputManager::GetInstance().Destroy();
+	ResourceManager::GetInstance().Destroy();
+	SceneManager::GetInstance().Destroy();
+	SoundManager::GetInstance().Destroy();
+
+	//// Effekseerを終了する。
+	//Effkseer_End();
+
+	// DxLib終了
+	if (DxLib_End() == -1)
+	{
+		isReleaseFail_ = true;
+	}
+
+	delete instance_;
+
+}
+
+bool Application::IsInitFail(void) const
+{
+	return isInitFail_;
+}
+
+bool Application::IsReleaseFail(void) const
+{
+	return isReleaseFail_;
+}
+
+Application::Application(void)
+{
+	isInitFail_ = false;
+	isReleaseFail_ = false;
+	isGameEnd_ = false;
+}
+
+
+/// <summary>
+/// ゲームを終了するか否か
+/// </summary>
+/// <param name="flag">ゲーム終了フラグ</param>
+void Application::SetGameEnd(bool flag)
+{
+	isGameEnd_ = flag;
+}
+
+void Application::InitEffekseer(void)
+{
+	/*if (Effekseer_Init(8000) == -1)
+	{
+		DxLib_End();
+	}
+	SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
+	Effekseer_SetGraphicsDeviceLostCallbackFunctions();*/
+}
