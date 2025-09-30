1264adab59e29bf274fa759dd2ba638f71d11146
kinoko0914 <ryusi.yamaguchi0914@gmail.com>
2025-09-30 23:54:56 +0900

    追加しなおし

---
+++ クロノエクスプローラー/Manager/SoundManager.h
@@ -0,0 +1,69 @@
+#pragma once
+#include<map>
+#include"Sound.h"
+
+class SoundManager
+{
+public:
+	//リソース名
+	enum class SRC
+	{
+		TITLE_BGM,		//タイトルシーンのBGM
+		SELECT_BGM,		//セレクトシーンのBGM
+		BTTLE_BGM_1,		//ゲームシーンのBGM
+		BTTLE_BGM_2,		//ゲームシーンのBGM
+		BTTLE_BGM_3,		//ゲームシーンのBGM
+		RESULT_BGM,		//リザルトシーンのBGM
+
+
+		ENTER_SOUND,	//決定音
+		SELECT_SOUND,	//選択しているときの効果音
+		DAMAGE_SOUND,	//ダメージを受けた時の効果音
+
+		IMPACT,
+
+		ATTRCK,
+		DAMAGE,
+		RELOAD,
+		DASH,
+		TURN,
+		FALL,
+	};
+
+	// 明示的にインステンスを生成する
+	static void CreateInstance(void);
+
+	// 静的インスタンスの取得
+	static SoundManager& GetInstance(void);
+
+	// 初期化
+	void Init(void);
+
+	// 解放(シーン切替時に一旦解放)
+	void Release(void);
+
+	// リソースの完全破棄
+	void Destroy(void);
+
+	bool Play(SRC src, Sound::TIMES times, bool isForce = false);	//二次元音源用
+	bool Play(SRC src, Sound::TIMES times, VECTOR pos, float radius);	//三次元音源用
+
+	void Stop(SRC src);	//音源を停止する
+	void AllStop(void);	//音源を停止する
+
+	bool CheckMove(SRC src);
+
+	void ChengeVolume(SRC src, float per);	//per 0.0～1.0でパーセントを設定する
+private:
+	// 静的インスタンス
+	static SoundManager* instance_;
+
+	std::map<SRC, Sound> soundMap_;
+
+	// デフォルトコンストラクタをprivateにして、
+	// 外部から生成できない様にする
+	SoundManager(void) = default;
+
+	// デストラクタも同様
+	~SoundManager(void) = default;
+};
---
+++ クロノエクスプローラー/Scene/SelectScene.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <DxLib.h>
+#include "../Application.h"
+#include "../Manager/ResourceManager.h"
+#include "../Manager/SceneManager.h"
+#include "../Manager/InputManager.h"
+#include "../Manager/SoundManager.h"
+
+#include "SelectScene.h"
+
+SelectScene::SelectScene()
+{
+}
+
+SelectScene::~SelectScene()
+{
+}
+
+
+void SelectScene::Init(void)
+{
+	//stage_ = SceneManager::GetInstance().GetStage();
+
+	//非同期読み込みを有効にする
+	SetUseASyncLoadFlag(false);
+}
+
+void SelectScene::Update(void)
+{
+	//ロードが完了したか判断
+	//if (GetASyncLoadNum() != 0 || SceneManager::GetInstance().IsLoading())
+	//{
+	//	return;
+	//}
+
+	//// シーン遷移
+	InputManager& ins = InputManager::GetInstance();
+	if (ins.IsTrgDown(KEY_INPUT_SPACE))
+	{
+		//SceneManager::GetInstance().ResetGame();
+		//SceneManager::GetInstance().SetStage(stage_);
+		SceneManager::GetInstance().ChangeScene(SceneManager::SCENE_ID::GAME);
+		return;
+	}
+
+	if (ins.IsTrgDown(KEY_INPUT_LEFT)) {
+		stage_--;
+	}
+	if (ins.IsTrgDown(KEY_INPUT_RIGHT)) {
+		stage_++;
+	}
+}
+
+void SelectScene::Draw(void)
+{
+	////ロードが完了したか判断
+	//if (GetASyncLoadNum() != 0 || SceneManager::GetInstance().IsLoading())
+	//{
+	//	return;
+	//}
+
+	DrawGraph(0, 0, ResourceManager::GetInstance().Load(ResourceManager::SRC::SELECT_IMG).handleId_, false);
+	DrawFormatString(0, 40, 0xff0000, "stage=%d", stage_);
+}
+
+void SelectScene::Release(void)
+{
+}
